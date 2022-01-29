4e015f7af674b2d07f96478b7e6203f71958bc6c
semir-t <semir.tursunovic@irqdesign.com>
2022-01-29 10:12:33 +0100

    GH pages

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main(void)
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;  								// enable the clock for GPIOD
     GPIOD->MODER |= 0x55000000;             							// set PORTD pin 12,13,14,15 as output
     GPIOD->OTYPER = 0x00000000;											// Output push-pull 
-    GPIOD->OSPEEDR |= 0xFF000000; 										// set max speed
+    GPIOD->OSPEEDR |= 0xFFF00000; 										// set max speed
 
 	GPIOD->ODR &= ~(0x000F << 12);										// Clear all LEDs
 	///wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww 	
